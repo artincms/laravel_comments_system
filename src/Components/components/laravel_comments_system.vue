fc406effb73cf4eb765756aa6b198ee8dfcc19b4
faramarz sadeghi <faramarz.sadeghi89@gmail.com>
2018-06-26 14:11:54 +0430

    fix

--- src/Components/components/laravel_comments_system.vue
+++ src/Components/components/laravel_comments_system.vue
@@ -14,12 +14,12 @@
         </v-bar>
         <div  id="mainForm" v-if="this.$store.state.canComment ">
             <div v-if="this.$store.state.quote_id != 0"  >
-            <div class="comment_content main_quote">
-                <button type="button" class="close close_open_reply" style="position: absolute;right: 7px;z-index: 10;" @click="closeQuote">×</button>
-                <div class="comment_area_right quote_picture">
+                <div class="comment_content main_quote">
+                    <button type="button" class="close close_open_reply" style="position: absolute;right: 7px;z-index: 10;" @click="closeQuote">×</button>
+                    <div class="comment_area_right quote_picture">
                         <img class="commenter_avatar" src="http://www.derayati.ir/FM/SRC/ID/-1/user_avatar.png">
                     </div>
-                        <div class="comment_area_left">
+                    <div class="comment_area_left">
                         <div class="comment_created_at" style="margin-right: 5%;">{{this.$store.state.data_array[this.$store.state.quote_id].created_at}}</div>
                         <h5>{{this.$store.state.data_array[this.$store.state.quote_id].name}}</h5>
                         <div>{{this.$store.state.data_array[this.$store.state.quote_id].comment}}</div>
@@ -37,7 +37,7 @@
 <script>
     import Item from './item.vue';
     import commentForm from './commentForm.vue';
-    import VBar from 'v-bar'
+    import VBar from '../../../../public/vendor/laravel_comments_system/packages/v-bar/src/components/v-bar.vue'
 
     export default {
         name:'laravel_comments_system',
@@ -48,28 +48,28 @@
             }
         },
         mounted () {
-           this.getData()
+            this.getData()
         },
         methods: {
             getData : function () {
                 axios.post("/LCS/getData", {model: this.target_model_name, id: this.target_id, pid_key: this.target_parent_column_name}).then((response) => {
                     this.treeData = response.data;
-                    this.$store.state.user_id = response.data.user_id ;
-                    this.$store.state.canComment = response.data.canComment ;
-                    this.$store.state.data_array = response.data.data_array ;
-                    this.$store.state.data = response.data ;
-                    if (response.data.lang =='en')
-                    {
-                        this.$translate.setLang("en");
-                    }
-                    else if (response.data.lang =='fa')
-                    {
-                        this.$translate.setLang("fa");
-                    } else
-                    {
-                        this.$translate.setLang("en");
-                    }
-                });
+                this.$store.state.user_id = response.data.user_id ;
+                this.$store.state.canComment = response.data.canComment ;
+                this.$store.state.data_array = response.data.data_array ;
+                this.$store.state.data = response.data ;
+                if (response.data.lang =='en')
+                {
+                    this.$translate.setLang("en");
+                }
+                else if (response.data.lang =='fa')
+                {
+                    this.$translate.setLang("fa");
+                } else
+                {
+                    this.$translate.setLang("en");
+                }
+            });
             },
             closeQuote : function () {
                 this.$store.state.quote_id = 0;
