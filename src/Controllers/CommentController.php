9b3083faf5335f4f293c2d3f98547695fc54d0af
faramarz sadeghi <faramarz.sadeghi89@gmail.com>
2018-10-14 15:49:48 +0330

    debug

--- src/Controllers/CommentController.php
+++ src/Controllers/CommentController.php
@@ -72,7 +72,7 @@ class CommentController extends Controller
                 }
                 else
                 {
-                    $data['children'] = LCS_BuildTree($comments > where('approved', '=', 1)->toArray(), $pid_key, false, false, 0, 'encode_id','children',$jalali_date);
+                    $data['children'] = LCS_BuildTree($comments -> where('approved', '=', 1)->toArray(), $pid_key, false, false,  LCS_getEncodeId(0), 'encode_id','children',$jalali_date);
 
                 }
             }
